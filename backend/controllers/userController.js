36341ea7bcb19de26a7359dda360b89516b06c20
santiagobarbera-png <santiago.barbera@gmail.com>
2026-02-17 19:19:18 -0300

    Create userController.js file with user management functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const getUserProfile = (req, res) => {
+    // Logic to get user profile
+};
+
+const updateUserProfile = (req, res) => {
+    // Logic to update user profile
+};
+
+const manageUserPreferences = (req, res) => {
+    // Logic to manage user preferences
+};
+
+const deleteUserAccount = (req, res) => {
+    // Logic to delete user account
+};
+
+module.exports = {
+    getUserProfile,
+    updateUserProfile,
+    manageUserPreferences,
+    deleteUserAccount
+};
